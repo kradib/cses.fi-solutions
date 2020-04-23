137583ad478f386ec4690ec710f32dc936338fc3
radib <radibkar32@gmail.com>
2020-04-24 01:17:00 +0530

    11.coin piles updated

--- 1.introductory Problems/11.coin_piles.cpp
+++ 1.introductory Problems/11.coin_piles.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define lli long long int
 int main(){
     int t;
     cin>>t;
     while(t--){
-        int a,b;
+        lli a,b;
         cin>>a>>b;
-        long long int sum=a+b;
+        lli sum=a+b;
         if(a==0 && b==0)
         cout<<"YES\n";
-        else if(a!=0 && b!=0 && abs(a-b)<=max(a,b) && (sum)%3==0)
+        else if(a!=0 && b!=0 && abs(a-b)<=min(a,b) && (sum)%3==0)
         cout<<"YES\n";
         else
         cout<<"NO\n";
